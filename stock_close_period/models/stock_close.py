bb0212f38b6f7af78af5b3424f1d3f2dcc949114
Borruso <gborruso@dinamicheaziendali.it>
2024-04-29 17:44:57 +0200

    [ADD] stock_close_period: added module for compute product pricing

---
+++ stock_close_period/models/stock_close.py
@@ -0,0 +1,288 @@
+# Copyright (C) 2023-Today:
+# Dinamiche Aziendali Srl (<http://www.dinamicheaziendali.it/>)
+# @author: Marco Calcagni <mcalcagni@dinamicheaziendali.it>
+# @author: Giuseppe Borruso <gborruso@dinamicheaziendali.it>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import logging
+from datetime import datetime
+
+from odoo import _, fields, models
+from odoo.exceptions import UserError
+
+logger = logging.getLogger(__name__)
+
+
+class StockClosePeriod(models.Model):
+    _name = "stock.close.period"
+    _description = "Stock Close Period"
+
+    name = fields.Char(
+        string="Reference",
+        readonly=True,
+        required=True,
+        states={"draft": [("readonly", False)], "confirm": [("readonly", False)]},
+    )
+    line_ids = fields.One2many(
+        "stock.close.period.line",
+        "close_id",
+        string="Product",
+        copy=True,
+        readonly=False,
+        states={"done": [("readonly", True)]},
+    )
+    state = fields.Selection(
+        [
+            ("draft", "Draft"),
+            ("confirm", "In Progress"),
+            ("done", "Validated"),
+            ("cancel", "Cancelled"),
+        ],
+        copy=False,
+        index=True,
+        readonly=True,
+        default="draft",
+        string="Status",
+    )
+    close_date = fields.Date(
+        readonly=True,
+        required=True,
+        default=fields.Date.context_today,
+        states={"draft": [("readonly", False)], "confirm": [("readonly", False)]},
+        help="The date that will be used for the store the product quantity and average cost.",
+    )
+    amount = fields.Float(string="Stock Amount Value", readonly=True, copy=False)
+    work_start = fields.Datetime(readonly=True, default=fields.Datetime.now)
+    work_end = fields.Datetime(readonly=True)
+    force_evaluation_method = fields.Selection(
+        [
+            ("no_force", "Compute based category setup"),
+            ("purchase", "Compute based purchase average cost"),
+            ("standard", "Compute based cost in product"),
+        ],
+        default="no_force",
+        copy=False,
+        help="Force Evaluation method will be used only for compute purchase costs.",
+    )
+    last_closed_id = fields.Many2one(
+        "stock.close.period",
+        string="Last Closed",
+        copy=False,
+        states={"done": [("readonly", True)]},
+    )
+    force_archive = fields.Boolean(
+        default=False,
+        help="Marks as archive the inventory move lines used during the process.",
+    )
+    purchase_ok = fields.Boolean(
+        default=False,
+        readonly=True,
+        help="Marks if action 'Compute Purchase' is processed.",
+    )
+    company_id = fields.Many2one(
+        "res.company", string="Company", default=lambda self: self.env.company
+    )
+
+    def unlink(self):
+        for closing in self:
+            if closing.state in ["confirm", "done"]:
+                raise UserError(
+                    _(
+                        "State in '%s'. You can only delete in state 'Draft' or 'Cancelled'."
+                        % closing.state
+                    )
+                )
+        return super(StockClosePeriod, self).unlink()
+
+    def action_set_to_draft(self):
+        for closing in self:
+            if closing.state == "cancel":
+                # clear data
+                closing.line_ids.unlink()
+                closing.state = "draft"
+                closing.amount = 0
+                closing.purchase_ok = False
+
+    def _get_product_lines(self):
+        self.ensure_one()
+
+        # add all products active not services type
+        query_add_products = """
+            INSERT INTO
+                stock_close_period_line(
+                    close_id,
+                    product_id,
+                    product_code,
+                    product_name,
+                    product_uom_id,
+                    categ_name,
+                    product_qty,
+                    price_unit,
+                    company_id
+                )
+            SELECT
+                %s AS close_id,
+                product_product.id AS product_id,
+                product_template.default_code AS product_code,
+                product_template.name AS product_name,
+                product_template.uom_id AS product_uom,
+                product_category.complete_name AS complete_name,
+                0 AS product_qty,
+                0 AS price_unit,
+                %s AS company_id
+            FROM
+                product_template,
+                product_product,
+                product_category
+            WHERE
+                product_template.type != 'service'
+                AND product_product.product_tmpl_id = product_template.id
+                AND product_template.categ_id = product_category.id
+                AND (
+                    product_template.company_id = %s
+                    OR product_template.company_id IS NULL
+                )
+            ORDER BY
+                product_product.id;
+        """
+        param_add_products = (
+            self.id,
+            self.company_id.id,
+            self.company_id.id,
+        )
+        self.env.cr.execute(query_add_products, param_add_products)
+
+        # get quantity on end period for each product
+        for closing_line_id in self.line_ids:
+            product_id = closing_line_id.product_id
+            list_product_qty = product_id._compute_qty_available(self.close_date)
+            count = 0
+            for line in list_product_qty:
+                if count == 0:
+                    closing_line_id.product_qty = line["stock_at_date"]
+                    closing_line_id.location_id = line["location_id"]
+                    closing_line_id.lot_id = line["lot_id"]
+                    closing_line_id.owner_id = line["owner_id"]
+                else:
+                    self.env["stock.close.period.line"].create(
+                        {
+                            "close_id": self.id,
+                            "product_id": line["product_id"],
+                            "product_uom_id": line["uom_id"],
+                            "product_qty": line["stock_at_date"],
+                            "location_id": line["location_id"],
+                            "lot_id": line["lot_id"],
+                            "owner_id": line["owner_id"],
+                            "price_unit": 0,
+                        }
+                    )
+                count += 1
+
+    def action_start(self):
+        for closing in self.filtered(lambda x: x.state not in ("done", "cancel")):
+            # add product line
+            closing._get_product_lines()
+            # set confirm status
+            closing.state = "confirm"
+        return True
+
+    def _check_qty_available(self):
+        self.ensure_one()
+
+        # if a negative value, can't continue
+        negative = self.line_ids.filtered(lambda x: x.product_qty < 0)
+        if negative:
+            res = False
+        else:
+            res = True
+        return res
+
+    def _deactivate_moves(self):
+        self.ensure_one()
+
+        # set active = False on stock_move and stock_move_line
+        query_move_inactive = """
+            UPDATE
+                stock_move
+            SET
+                active = false
+            WHERE
+                date <= %s
+                AND state = 'done'
+                AND (
+                    company_id == %s
+                    OR company_id IS NULL
+                );
+        """
+        param_move_inactive = (
+            self.close_date,
+            self.company_id.id,
+        )
+        self.env.cr.execute(query_move_inactive, param_move_inactive)
+
+        query_move_line_inactive = """
+            UPDATE
+                stock_move_line
+            SET
+                active = false
+            WHERE
+                date <= %s
+                AND state = 'done'
+                AND (
+                    company_id == %s
+                    OR company_id IS NULL
+                );
+        """
+        param_move_line_inactive = (
+            self.close_date,
+            self.company_id.id,
+        )
+        self.env.cr.execute(query_move_line_inactive, param_move_line_inactive)
+        return True
+
+    def action_recalculate_purchase(self):
+        for closing in self:
+            if not closing._check_qty_available():
+                raise UserError(
+                    _(
+                        "Is not possible continue the execution. "
+                        "There are product with quantities < 0."
+                    )
+                )
+
+            self.env["stock.move.line"].recompute_average_cost_period_purchase(closing)
+            closing.purchase_ok = True
+            if closing.force_archive:
+                closing._deactivate_moves()
+            closing.work_end = datetime.now()
+        return True
+
+    def action_cancel(self):
+        for closing in self:
+            closing.state = "cancel"
+        return True
+
+    def action_force_done(self):
+        for closing in self:
+            closing.state = "done"
+            closing.amount = sum(closing.mapped("line_ids.amount_line"))
+
+    def action_done(self):
+        for closing in self:
+            closing.state = "done"
+            closing.amount = sum(closing.mapped("line_ids.amount_line"))
+            query_delete_zero_line = """
+                DELETE FROM
+                    stock_close_period_line
+                WHERE
+                    close_id = %s
+                    AND product_qty = 0
+                    AND price_unit = 0;
+            """
+            param_delete_zero_line = (closing.id,)
+            self.env.cr.execute(query_delete_zero_line, param_delete_zero_line)
+        return True
+
+    def action_recompute_amount(self):
+        for closing in self:
+            closing.amount = sum(closing.mapped("line_ids.amount_line"))
---
+++ stock_close_period/wizards/stock_close_import.py
@@ -0,0 +1,99 @@
+# Copyright (C) 2023-Today:
+# Dinamiche Aziendali Srl (<http://www.dinamicheaziendali.it/>)
+# @author: Marco Calcagni <mcalcagni@dinamicheaziendali.it>
+# @author: Giuseppe Borruso <gborruso@dinamicheaziendali.it>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import base64
+import logging
+from datetime import datetime
+
+import unicodecsv
+
+from odoo import _, fields, models
+from odoo.exceptions import UserError
+
+_logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+class StockCloseImportWizard(models.TransientModel):
+    _name = "stock.close.import.wizard"
+    _description = "Stock Close Import Wizard"
+
+    file = fields.Binary()
+    close_id = fields.Many2one("stock.close.period", string="Stock Close Period")
+
+    def load_products(self, lines):
+        products = {}
+        for row in lines:
+            default_code = row["CODE"]
+            product_obj = self.env["product.product"].search(
+                [("default_code", "=", default_code)], limit=1
+            )
+            if not product_obj:
+                raise UserError(_("Product %s not found") % default_code)
+            products[default_code] = product_obj[0]
+        return products
+
+    def import_csv(self):
+        # set done close_id
+        self.close_id.work_start = datetime.now()
+
+        try:
+            file_to_import = base64.decodebytes(self.file).splitlines()
+            reader = unicodecsv.reader(file_to_import, encoding="utf-8", delimiter=";")
+            lines = []
+            headers = False
+
+            for row in reader:
+                headers = row
+                break
+
+            parsed_data_lines = unicodecsv.DictReader(
+                file_to_import, fieldnames=headers, encoding="utf-8", delimiter=";"
+            )
+
+            for index, row in enumerate(parsed_data_lines):
+                if index == 0:
+                    continue
+                lines.append(
+                    {
+                        "CODE": str(row["CODE"]),
+                        "COST": str(row["COST"]).replace(",", "."),
+                        "QTY": str(row["QTY"]).replace(",", "."),
+                    }
+                )
+            products = self.load_products(lines)
+            total = 0.0
+            dp_qty = 4
+            dp_price = 5
+            for row in lines:
+                product_id = products[row["CODE"]].id
+                unit_cost = round(float(row["COST"]), dp_price)
+                qty = round(float(row["QTY"]), dp_qty)
+                total += unit_cost * qty
+                self.env["stock.close.period.line"].with_context(
+                    tracking_disable=True
+                ).create(
+                    {
+                        "close_id": self.close_id.id,
+                        "product_id": product_id,
+                        "price_unit": unit_cost,
+                        "product_qty": qty,
+                        "product_uom_id": products[
+                            row["CODE"]
+                        ].product_tmpl_id.uom_id.id,
+                        "evaluation_method": "",
+                    }
+                )
+
+            # set done close_id
+            self.close_id.amount = total
+            self.close_id.work_end = datetime.now()
+            self.close_id.state = "done"
+
+        except Exception as e:
+            raise UserError(e)
